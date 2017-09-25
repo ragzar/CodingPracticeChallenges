7735b47e3aeae2ac69913b75302af8c394953727
Edgar Ruiz <edgar.ruiz@prodigious.com>
2017-09-25 12:26:41 -0500

    Creating coding project

---
+++ CodingPracticeChallenges/src/co/hakerrank/challenges/thirtydaysofcode/Day7.java
@@ -0,0 +1,16 @@
+package co.hakerrank.challenges.thirtydaysofcode;
+import java.util.Scanner;
+
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+    }
+}
